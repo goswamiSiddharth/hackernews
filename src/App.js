105eb0aeb7e8cdc0e8d58b58ae355720c847919a
goswamiSiddharth <siddharth03goswami@gmail.com>
2023-12-26 00:08:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import format from 'date-fns';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState('programming');
+  const [text, setText] = useState('');
+  const [largeTitle, setLargeTitle] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const res = await fetch(` http://hn.algolia.com/api/v1/search?query=${query}`);
+      const data = await res.json();
+      console.log(data);
+      setItems(data.hits);
+      setLargeTitle(data.hits[0]);
+    }
+     
+    fetchArticles();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section">
+        <form autoComplete="off">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search for Something"
+          ></input>
+          <button>Search</button>
+        </form>
+        <article className="title">
+          <h1>{largeTitle.title}</h1>
+          <a href="">Read full Article</a>
+        </article>
+        <article className="cards">
+          <div>
+            <h2>Heading 2</h2>
+            <ul>
+              <li>By Siddharth</li>
+              <li>
+                <a href="">Read full Article</a>
+              </li>
+            </ul>
+            <p>Data</p>
+          </div>
+        </article>
+      </section>
+    </>
   );
 }
 
